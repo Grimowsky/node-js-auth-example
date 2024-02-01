0393b7ef8c6b88bc6b7c44e70f2132667d91ca8a
Grimm <rafalmirkowski@gmail.com>
2024-02-01 16:53:57 +0100

    fix user routes

--- src/api/rest/v1/Routes/User.route.ts
+++ src/api/rest/v1/Routes/User.route.ts
@@ -1,10 +1,7 @@
 import * as express from 'express';
-import * as UserController from '../Controllers/User.controller';
 
 const createRouter = (): express.Router => {
     const router = express.Router();
-
-    router.use('/register', UserController.register);
 
     return router;
 };
