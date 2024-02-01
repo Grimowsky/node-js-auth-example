d6819e1f70e5fa9200b8796dd47c976c9e891b47
Grimm <rafalmirkowski@gmail.com>
2024-02-01 16:54:43 +0100

    add missing controller

--- src/api/rest/v1/Routes/Register.route.ts
+++ src/api/rest/v1/Routes/Register.route.ts
@@ -1,9 +1,9 @@
 import * as express from 'express';
-
+import * as RegistrationController from '../Controllers/Registration.controller';
 const createRouter = (): express.Router => {
     const router = express.Router();
 
-    router.post('/register');
+    router.post('/register', RegistrationController.register);
 
     return router;
 };
