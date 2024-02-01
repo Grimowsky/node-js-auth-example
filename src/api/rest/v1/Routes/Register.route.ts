32289f4aea2d9d5bf69866ae31c97af0b6612e19
Grimm <rafalmirkowski@gmail.com>
2024-02-01 17:03:58 +0100

    generate prisma repo

--- src/api/rest/v1/Routes/Register.route.ts
+++ src/api/rest/v1/Routes/Register.route.ts
@@ -2,7 +2,6 @@ import * as express from 'express';
 import * as RegistrationController from '../Controllers/Registration.controller';
 const createRouter = (): express.Router => {
     const router = express.Router();
-
     router.post('/register', RegistrationController.register);
 
     return router;
