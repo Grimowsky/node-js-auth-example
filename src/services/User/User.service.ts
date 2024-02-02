a68600f2c1f6c0bf76ea2f13dd63b11bfd3a0b1b
Grimm <rafalmirkowski@gmail.com>
2024-02-02 14:16:54 +0100

    add user registration with hashed password

--- src/services/User/User.service.ts
+++ src/services/User/User.service.ts
@@ -2,16 +2,29 @@ import { type User } from '@services/User/User.type';
 import UserRepository from '@repository/User';
 import { ExtendedError } from '../../utils/error/error';
 import { StatusCodes } from 'http-status-codes';
+import * as bcrypt from 'bcrypt';
+
+// user request
+// username || email exits -> error
+// register with encrypted password
 
 const register = async (data: User): Promise<User> => {
     if (
         (await UserRepository.findByEmail(data.email)) ||
         (await UserRepository.findByUserName(data.username))
     ) {
-        throw ExtendedError.of('user taken', StatusCodes.BAD_REQUEST);
+        throw ExtendedError.of(
+            'Username or email has been taken',
+            StatusCodes.BAD_REQUEST
+        );
     }
 
-    return await UserRepository.create(data);
+    const userWithHashedPass: User = {
+        ...data,
+        password: await bcrypt.hash(data.password, 10),
+    };
+
+    return await UserRepository.create(userWithHashedPass);
 };
 
 const userService = { register };
