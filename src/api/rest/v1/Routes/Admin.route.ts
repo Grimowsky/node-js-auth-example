54e9251f79027a4daf835a50cd24c0f1d458ccea
Grimm <rafalmirkowski@gmail.com>
2024-02-23 15:55:37 +0100

    add roles middleware

--- src/api/rest/v1/Routes/Admin.route.ts
+++ src/api/rest/v1/Routes/Admin.route.ts
@@ -1,10 +1,17 @@
 import * as express from 'express';
 import { asyncWrapper } from '../../../../middleware/asyncWrapper';
 import * as AdminController from '../Controllers/Admin.controller';
+import jwtMiddleware from '../../../../middleware/jwtMiddleware';
+import roleMiddleware from '../../../../middleware/roleMiddleware';
 const createRouter = (): express.Router => {
     const router = express.Router();
 
-    router.get('/users/list', asyncWrapper(AdminController.usersList));
+    router.get(
+        '/users/list',
+        jwtMiddleware.verifyAuthToken,
+        roleMiddleware(['admin']),
+        asyncWrapper(AdminController.usersList)
+    );
 
     return router;
 };
