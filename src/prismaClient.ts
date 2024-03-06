dc4b66230faf77ead48621db5dea122069cab731
Grimm <rafalmirkowski@gmail.com>
2024-03-06 09:16:06 +0100

    fix prisma client refference in singleton

--- src/prismaClient.ts
+++ src/prismaClient.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+//it has to be generated client instead of @prisma/client from node-modules
+import { PrismaClient } from './prisma/client';
 
 const prisma = new PrismaClient();
 export default prisma;
